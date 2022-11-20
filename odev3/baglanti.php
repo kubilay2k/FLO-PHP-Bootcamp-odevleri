3b5d678be30939362e77bcfeb826a7bc9b24b065
Kubilay <80216892+kubilay2k@users.noreply.github.com>
2022-11-20 18:33:25 +0300

    Update baglanti.php

--- odev3/baglanti.php
+++ odev3/baglanti.php
@@ -1,7 +1,8 @@
 <?php 
-
-$baglanti = new PDO('mysql:host=localhost;dbname=bootcamp;charset:utf8', 'root', '');
-
-$baglanti->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
+try{
+    $baglanti = new PDO('mysql:host=localhost;dbname=bootcamp;charset:utf8', 'root', '');
+    $baglanti->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  } catch (Exception $e) {
+    echo 'Hata!: '. $e->getMessage();
+}
 ?>
